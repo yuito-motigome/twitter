3244705df52bd9a4824420e172917e63b09cb5a7
yuito-motigome <uemoriyuito20071204@gmail.com>
2020-06-29 18:05:02 +0800

    post new hometopヘッダーない

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,13 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all
     end    
+    def new
+    end    
+    def create
+        post = Post.new(content: params[:content])
+        if post.save
+            redirect_to("/posts/index")
+        end    
+        
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get '/' =>"home#top"
   get "about" => "home#about"
   get "posts/index" => "posts#index"
+  get "posts/new" => "posts#new"
+  post "posts/create" => "posts#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
