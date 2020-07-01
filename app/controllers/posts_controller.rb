1d5200154141a244d6802639f38359b70374f6a6
yuito-motigome <uemoriyuito20071204@gmail.com>
2020-07-01 17:16:33 +0800

    トップページ修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,13 +2,20 @@ class PostsController < ApplicationController
     def index
         @posts = Post.all
     end    
+    
     def new
     end    
+    
     def create
         post = Post.new(content: params[:content])
-        if post.save
-            redirect_to("/posts/index")
-        end    
-        
+        if post != nil    
+            if post.save
+                redirect_to("/posts/index")
+            end    
+        end
     end
+    
+    def show
+        @post = Post.find_by(id: params[:id])
+    end    
 end
